e2d4b9a7720411246a0f74fe1b7caa858ad536cd
AteeqAkbar <56605324+AteeqAkbar@users.noreply.github.com>
2023-06-05 18:54:18 +0500

    add to git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationModule } from '@progress/kendo-angular-navigation';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { IndicatorsModule } from '@progress/kendo-angular-indicators';
+import { PopupModule } from '@progress/kendo-angular-popup';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { IconsModule } from '@progress/kendo-angular-icons';
+import { HeroComponent } from './components/hero/hero.component';
+
+
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonsModule,
+    BrowserAnimationsModule,
+    NavigationModule,
+    LayoutModule,
+    IndicatorsModule,
+    PopupModule,
+    InputsModule,
+    IconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
