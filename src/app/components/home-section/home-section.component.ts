4b4a27c585d5dda1632067f2dbf51510926e1967
AteeqAkbar <56605324+AteeqAkbar@users.noreply.github.com>
2023-06-08 14:08:54 +0500

    figma

--- src/app/components/home-section/home-section.component.ts
+++ src/app/components/home-section/home-section.component.ts
@@ -12,19 +12,19 @@ export class HomeSectionComponent {
       text: 'The most accurate and documented estimates in the industry',
     },
     {
-      icon: 'assets/Icons1.svg',
+      icon: 'assets/Icons2.svg',
       text: 'Representation of the full scope of the project',
     },
     {
-      icon: 'assets/Icons1.svg',
+      icon: 'assets/Icons3.svg',
       text: 'Time-saving estimates and communication with the carrier',
     },
     {
-      icon: 'assets/Icons1.svg',
+      icon: 'assets/Icons4.svg',
       text: 'Proper project valuation',
     },
     {
-      icon: 'assets/Icons1.svg',
+      icon: 'assets/Icons5.svg',
       text: 'Automated process management with our free app',
     },
   ];
