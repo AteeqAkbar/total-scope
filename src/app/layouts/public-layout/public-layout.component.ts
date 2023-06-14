85d70a6a9c1a07812431c6b8047250e2c71be402
AteeqAkbar <56605324+AteeqAkbar@users.noreply.github.com>
2023-06-14 11:04:55 +0500

    fix style

--- src/app/layouts/public-layout/public-layout.component.ts
+++ src/app/layouts/public-layout/public-layout.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'public-layout',
   templateUrl: './public-layout.component.html',
+  styleUrls: ['./public-layout.component.scss'],
 })
 export class PublicLayoutComponent implements OnInit {
   constructor() {}
