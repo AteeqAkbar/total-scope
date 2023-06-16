0e5c841a2a1f842cf9ad3c196ecca64b46a3d9ab
AteeqAkbar <56605324+AteeqAkbar@users.noreply.github.com>
2023-06-16 16:06:12 +0500

    fix error and about page ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { ButtonsModule } from '@progress/kendo-angular-buttons'
-import { IconsModule } from '@progress/kendo-angular-icons'
-import { IndicatorsModule } from '@progress/kendo-angular-indicators'
-import { InputsModule } from '@progress/kendo-angular-inputs'
-import { LayoutModule } from '@progress/kendo-angular-layout'
-import { NavigationModule } from '@progress/kendo-angular-navigation'
-import { PopupModule } from '@progress/kendo-angular-popup'
-import { ApiService } from 'src/core/api.service'
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { FooterComponent, NavbarComponent, PublicLayoutComponent } from './layouts'
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { IconsModule } from '@progress/kendo-angular-icons';
+import { IndicatorsModule } from '@progress/kendo-angular-indicators';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { NavigationModule } from '@progress/kendo-angular-navigation';
+import { PopupModule } from '@progress/kendo-angular-popup';
+import { ApiService } from 'src/core/api.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {
+  FooterComponent,
+  NavbarComponent,
+  PublicLayoutComponent,
+} from './layouts';
 
 @NgModule({
-  declarations: [AppComponent, PublicLayoutComponent, NavbarComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    PublicLayoutComponent,
+    NavbarComponent,
+    FooterComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
